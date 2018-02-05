d78b6a51e95687593f0a7440a06c144ca9676585
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-02-05 17:33:03 -0600

    movies displaying in grid

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -14,4 +14,9 @@ angular.module("mixtape")
         key: "fdcfd222077448d9b1f7f9967fc6f178",
         secret: "80f8276cd4304917bb153cc2f42a352c",
         url: "https://api.spotify.com/v1"
+    })
+    .constant("TMDB", {
+        image_prefix: "https://image.tmdb.org/t/p/w400_and_h600_bestv2",
+        url: "https://api.themoviedb.org/3",
+        key: "d7208980a35f7aef364e81fcb05147a4"
     });
--- assets/js/ctrl/home.js
+++ assets/js/ctrl/home.js
@@ -2,10 +2,11 @@
 
 const angular = require("angular");
 
-angular.module("mixtape").controller("homeCtrl", function($scope, $routeParams, $location, SpotifyUserFactory, MediaFactory) {
+angular.module("mixtape").controller("homeCtrl", function($scope, $routeParams, $location, SpotifyUserFactory, MediaFactory, TMDB) {
     MediaFactory.getRecentMovies(4)
         .then(data => {
             $scope.movies = data;
+            $scope.moviePrefix = TMDB.image_prefix;
         })
         .catch(err => console.log(err));
 });
