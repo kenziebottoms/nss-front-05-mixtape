6c25235d5ef554e0c257edb6096d5f1b1cd795fa
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-02-07 10:51:54 -0600

    active toggling works, and search button is disabled if no mediums are selected

--- assets/js/ctrl/newLink.js
+++ assets/js/ctrl/newLink.js
@@ -3,15 +3,4 @@
 const angular = require("angular");
 
 angular.module("mixtape").controller("NewLinkCtrl", function($scope) {
-    $scope.setActive = mediaType => {
-        $scope.active = mediaType;
-    };
-
-    $scope.isActive = mediaType => {
-        if ($scope.active == mediaType) {
-            return "active";
-        } else {
-            return "";
-        }
-    };
 });
