033c036ca8edd4396bba4df9a94aa6d7cb84073d
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-02-07 11:59:37 -0600

    all kinds of search results are loadding

--- assets/js/ctrl/newLink.js
+++ assets/js/ctrl/newLink.js
@@ -2,7 +2,7 @@
 
 const angular = require("angular");
 
-angular.module("mixtape").controller("NewLinkCtrl", function($scope, GoodreadsFactory, TmdbFactory, SpotifyAuthFactory, $location) {
+angular.module("mixtape").controller("NewLinkCtrl", function($scope, GoodreadsFactory, TmdbFactory, SpotifyAuthFactory, $location, TMDB) {
     SpotifyAuthFactory.getActiveUserData()
         .then(data => {
         })
@@ -15,8 +15,19 @@ angular.module("mixtape").controller("NewLinkCtrl", function($scope, GoodreadsFa
             if ($scope.active == "books") {
                 GoodreadsFactory.searchByTitle($scope.mediaSearchTerm)
                     .then(results => {
-                        console.log(results);
                         $scope.results = results.results.work.slice(0,5);
+                    });
+            } else if ($scope.active == "tv") {
+                $scope.image_prefix = TMDB.small_image_prefix;
+                TmdbFactory.searchTvShowsByTitle($scope.mediaSearchTerm)
+                    .then(results => {
+                        $scope.results = results.results.slice(0,5);
+                    });
+            } else if ($scope.active == "movies") {
+                $scope.image_prefix = TMDB.small_image_prefix;
+                TmdbFactory.searchMoviesByTitle($scope.mediaSearchTerm)
+                    .then(results => {
+                        $scope.results = results.results.slice(0,5);
                     });
             }
         }
