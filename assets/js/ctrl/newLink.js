8839c6d0a02bda0bdbc69df63a1bebfa17c141b8
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-02-07 14:53:13 -0600

    song search works and is formatted

--- assets/js/ctrl/newLink.js
+++ assets/js/ctrl/newLink.js
@@ -2,7 +2,7 @@
 
 const angular = require("angular");
 
-angular.module("mixtape").controller("NewLinkCtrl", function($scope, GoodreadsFactory, TmdbFactory, SpotifyAuthFactory, $location, TMDB) {
+angular.module("mixtape").controller("NewLinkCtrl", function($scope, GoodreadsFactory, TmdbFactory, SpotifyAuthFactory, $location, TMDB, SpotifySearchFactory) {
     SpotifyAuthFactory.getActiveUserData()
         .then(data => {
         })
@@ -16,19 +16,30 @@ angular.module("mixtape").controller("NewLinkCtrl", function($scope, GoodreadsFa
             if ($scope.activeMedia == "books") {
                 GoodreadsFactory.searchByTitle($scope.mediaSearchTerm)
                     .then(results => {
-                        $scope.results = results.results.work.slice(0,5);
+                        $scope.mediaResults = results.results.work.slice(0,5);
                     });
             } else if ($scope.activeMedia == "tv") {
                 $scope.image_prefix = TMDB.small_image_prefix;
                 TmdbFactory.searchTvShowsByTitle($scope.mediaSearchTerm)
                     .then(results => {
-                        $scope.results = results.results.slice(0,5);
+                        $scope.mediaResults = results.results.slice(0,5);
                     });
             } else if ($scope.activeMedia == "movies") {
                 $scope.image_prefix = TMDB.small_image_prefix;
                 TmdbFactory.searchMoviesByTitle($scope.mediaSearchTerm)
                     .then(results => {
-                        $scope.results = results.results.slice(0,5);
+                        $scope.mediaResults = results.results.slice(0,5);
+                    });
+            }
+        }
+    };
+
+    $scope.searchMusic = () => {
+        if ($scope.searchMusicTerm != "" && $scope.activeMusic) {
+            if ($scope.activeMusic == "tracks") {
+                SpotifySearchFactory.searchTracksByTitle($scope.musicSearchTerm)
+                    .then(results => {
+                        $scope.musicResults = results.slice(0,5);
                     });
             }
         }
