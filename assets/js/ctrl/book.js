de5740edc825b25c251c2bc735c4f733570c0dd4
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-02-07 11:44:56 -0600

    5 book results populate

--- assets/js/ctrl/book.js
+++ assets/js/ctrl/book.js
@@ -1,18 +1,12 @@
 "use strict";
 
 const angular = require("angular");
-const convert = require('xml-js');
 
 angular.module("mixtape").controller("BookCtrl", function($scope, GoodreadsFactory, $routeParams, GOODREADS, LinkFactory, FirebaseFactory) {
     $scope.id = $routeParams.id;
     // get book details
     GoodreadsFactory.getBookById($scope.id)
-        .then(data => {
-            // convert XML to JSON
-            data = JSON.parse(convert.xml2json(data, {compact: true}));
-            // shave down JSON
-            let book = data.GoodreadsResponse.book;
-
+        .then(book => {
             // update cached info in Firebase
             FirebaseFactory.cacheInfo(`book:${$scope.id}`, GoodreadsFactory.parseApiInfo(book));
 
--- assets/js/ctrl/newLink.js
+++ assets/js/ctrl/newLink.js
@@ -2,5 +2,23 @@
 
 const angular = require("angular");
 
-angular.module("mixtape").controller("NewLinkCtrl", function($scope) {
+angular.module("mixtape").controller("NewLinkCtrl", function($scope, GoodreadsFactory, TmdbFactory, SpotifyAuthFactory, $location) {
+    SpotifyAuthFactory.getActiveUserData()
+        .then(data => {
+        })
+        .catch(err => {
+            $location.path("/");
+        });
+
+    $scope.search = () => {
+        if ($scope.mediaSearchTerm != "") {
+            if ($scope.active == "books") {
+                GoodreadsFactory.searchByTitle($scope.mediaSearchTerm)
+                    .then(results => {
+                        console.log(results);
+                        $scope.results = results.results.work.slice(0,5);
+                    });
+            }
+        }
+    };
 });
