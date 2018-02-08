02ee0f800184c884d449a407a4dac6403a5d7b27
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-02-08 15:22:56 -0600

    changed homepage limit to 5

--- assets/js/ctrl/home.js
+++ assets/js/ctrl/home.js
@@ -3,18 +3,18 @@
 const angular = require("angular");
 
 angular.module("mixtape").controller("HomeCtrl", function($scope, $routeParams, $location, SpotifyAuthFactory, FirebaseFactory, TMDB) {
-    FirebaseFactory.getMediaByType("movie", 4)
+    FirebaseFactory.getMediaByType("movie", 5)
         .then(data => {
             $scope.movies = data;
         })
         .catch(err => console.log(err));
     
-    FirebaseFactory.getMediaByType("tv", 4)
+    FirebaseFactory.getMediaByType("tv", 5)
         .then(data => {
             $scope.tvShows = data;
         })
         .catch(err => console.log(err));
-    FirebaseFactory.getMediaByType("book", 4)
+    FirebaseFactory.getMediaByType("book", 5)
         .then(data => {
             $scope.books = data;
         });
