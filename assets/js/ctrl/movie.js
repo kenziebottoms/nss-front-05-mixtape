d9c08e335cbd1a482377df80d34692cdaf5ab496
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-02-12 13:42:59 -0600

    delete button works on movie

--- assets/js/ctrl/movie.js
+++ assets/js/ctrl/movie.js
@@ -3,20 +3,36 @@
 const angular = require("angular");
 
 angular.module("mixtape").controller("MovieCtrl", function($scope, TmdbFactory, $routeParams, TMDB, LinkFactory, FirebaseFactory) {
+    let fetchInfo = (typeId) => {
+        LinkFactory.getLinksByMedia(typeId)        
+            .then(loadedLinks => {
+                $scope.links = loadedLinks;
+                $scope.context = "media";
+            });
+    };
+    let getLinks = typeId => {
+        TmdbFactory.getMovieById($scope.id)
+            .then(movie => {
+                // update cached info in Firebase
+                movie =  TmdbFactory.parseApiInfo("movie", movie);
+                FirebaseFactory.storeMedia(typeId, movie);
+
+                // pass data to dom
+                $scope.media = movie;
+            });
+    };
+
     $scope.id = $routeParams.id;
     let typeId = `movie:${$scope.id}`;
-    TmdbFactory.getMovieById($scope.id)
-        .then(movie => {
-            // update cached info in Firebase
-            movie =  TmdbFactory.parseApiInfo("movie", movie);
-            FirebaseFactory.storeMedia(typeId, movie);
+    fetchInfo(typeId);
+    getLinks(typeId);
 
-            // pass data to dom
-            $scope.media = movie;
-        });
-    LinkFactory.getLinksByMedia(typeId)        
-        .then(loadedLinks => {
-            $scope.links = loadedLinks;
-            $scope.context = "media";
-        });
+    $scope.deleteLink = key => {
+        LinkFactory.deleteLink(key)
+            .then(result => {
+            })
+            .catch(err => {
+                Materialize.toast(err, 3000);
+            });
+    };
 });
