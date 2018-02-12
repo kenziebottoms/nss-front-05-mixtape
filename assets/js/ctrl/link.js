bd9560de96ceb99ae45e25320eba16fb75be2367
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-02-12 11:02:25 -0600

    commented link

--- assets/js/ctrl/link.js
+++ assets/js/ctrl/link.js
@@ -2,7 +2,8 @@
 
 const angular = require("angular");
 
-angular.module("mixtape").controller("LinkCtrl", function($scope, GoodreadsFactory, TmdbFactory, SpotifyAuthFactory, $location, TMDB, SpotifyTrackFactory, FirebaseFactory, LinkFactory, $window, $routeParams, SpotifyPlaylistFactory, $http, $q) {
+angular.module("mixtape").controller("LinkCtrl", function ($scope, GoodreadsFactory, TmdbFactory, SpotifyAuthFactory, $location, TMDB, SpotifyTrackFactory, FirebaseFactory, LinkFactory, $window, $routeParams, SpotifyPlaylistFactory) {
+
     // get active user
     SpotifyAuthFactory.getActiveUserData()
         .then(data => {
@@ -14,9 +15,11 @@ angular.module("mixtape").controller("LinkCtrl", function($scope, GoodreadsFacto
                 // grab the link
                 LinkFactory.getLinkByKey($scope.key)
                     .then(link => {
+                        // if user didn't create the link
                         if ($scope.user.id != link.uid) {
                             $location.path("/");
                         } else {
+                            // populate page with existing link's contents
                             $scope.activeMedia = link.media.split(":")[0];
                             $scope.activeMusic = link.music.split(":")[0];
                             $scope.tags = link.tags.join(", ");
@@ -30,6 +33,7 @@ angular.module("mixtape").controller("LinkCtrl", function($scope, GoodreadsFacto
                                 });
                         }
                     });
+                // if adding a new page
             } else {
                 $scope.context = "new";
             }
@@ -39,36 +43,38 @@ angular.module("mixtape").controller("LinkCtrl", function($scope, GoodreadsFacto
             $location.path("/");
         });
 
+    // takes the search term and the search medium and displays 5 results
     $scope.searchMedia = () => {
         if ($scope.mediaSearchTerm != "" && $scope.activeMedia) {
             $scope.results = {};
             if ($scope.activeMedia == "book") {
                 GoodreadsFactory.searchByTitle($scope.mediaSearchTerm)
                     .then(results => {
-                        $scope.mediaResults = results.results.work.slice(0,5);
+                        $scope.mediaResults = results.results.work.slice(0, 5);
                     });
             } else if ($scope.activeMedia == "tv") {
                 $scope.image_prefix = TMDB.small_image_prefix;
                 TmdbFactory.searchTvShowsByTitle($scope.mediaSearchTerm)
                     .then(results => {
-                        $scope.mediaResults = results.results.slice(0,5);
+                        $scope.mediaResults = results.results.slice(0, 5);
                     });
             } else if ($scope.activeMedia == "movie") {
                 $scope.image_prefix = TMDB.small_image_prefix;
                 TmdbFactory.searchMoviesByTitle($scope.mediaSearchTerm)
                     .then(results => {
-                        $scope.mediaResults = results.results.slice(0,5);
+                        $scope.mediaResults = results.results.slice(0, 5);
                     });
             }
         }
     };
 
+    // takes the search term and the search medium and displays 5 results
     $scope.searchMusic = () => {
         if ($scope.searchMusicTerm != "" && $scope.activeMusic) {
             if ($scope.activeMusic == "track") {
                 SpotifyTrackFactory.searchTracksByTitle($scope.musicSearchTerm)
                     .then(results => {
-                        $scope.musicResults = results.slice(0,5);
+                        $scope.musicResults = results.slice(0, 5);
                     });
             } else {
                 SpotifyPlaylistFactory.searchUserPlaylists($scope.user.id, $scope.musicSearchTerm, 50, 0)
@@ -80,6 +86,7 @@ angular.module("mixtape").controller("LinkCtrl", function($scope, GoodreadsFacto
         }
     };
 
+    // searches the next batch of 50 playlists for the search term
     $scope.searchMorePlaylists = () => {
         SpotifyPlaylistFactory.searchUserPlaylists($scope.user.id, $scope.musicSearchTerm, 50, $scope.offset)
             .then(results => {
@@ -88,6 +95,7 @@ angular.module("mixtape").controller("LinkCtrl", function($scope, GoodreadsFacto
             });
     };
 
+    // select media and display info bigger
     $scope.selectMedia = (id) => {
         if ($scope.activeMedia == "tv") {
             TmdbFactory.getTvShowById(id)
@@ -107,6 +115,7 @@ angular.module("mixtape").controller("LinkCtrl", function($scope, GoodreadsFacto
         }
     };
 
+    // select music and display info bigger
     $scope.selectMusic = (id) => {
         if ($scope.activeMusic == "track") {
             SpotifyTrackFactory.getTrackById(id)
@@ -122,11 +131,14 @@ angular.module("mixtape").controller("LinkCtrl", function($scope, GoodreadsFacto
         }
     };
 
+    // submit mix
     $scope.submit = () => {
+        // if you've picked music and media
         if ($scope.selectedMedia && $scope.selectedMusic) {
             let mediaTypeId = `${$scope.activeMedia}:${$scope.selectedMedia.id}`;
             let musicTypeId = `${$scope.activeMusic}:${$scope.selectedMusic.id}`;
             let promises = [];
+            // store the media by itself
             promises.push(FirebaseFactory.storeMedia(mediaTypeId, $scope.selectedMedia));
             if (musicTypeId.split(":")[0] == "playlist") {
                 musicTypeId = `${$scope.user.id}:${musicTypeId.split(":")[1]}`;
@@ -134,18 +146,25 @@ angular.module("mixtape").controller("LinkCtrl", function($scope, GoodreadsFacto
             if ($scope.activeMusic == "playlist") {
                 musicTypeId = `playlist:${musicTypeId}`;
             }
+            // store music by itself
             promises.push(FirebaseFactory.storeMusic(musicTypeId, $scope.selectedMusic));
+
+            // after music and media have been stored
             Promise.all(promises)
                 .then(response => {
                     if ($scope.context == "new") {
+                        // create new link
                         return LinkFactory.storeNewLink(mediaTypeId, musicTypeId, $scope.tags.trim().split(","), $scope.user.id);
+                        // patch existing link
                     } else {
                         return LinkFactory.editLink($scope.key, mediaTypeId, musicTypeId, $scope.tags.trim().split(","), $scope.user.id);
                     }
                 })
+                // success => redirect ot media page
                 .then(response => {
                     $window.location.href = `#!/${$scope.activeMedia}/${mediaTypeId.split(":")[1]}`;
                 })
+                // oh shit message
                 .catch(err => {
                     Materialize.toast(err, 3000, "pink accent-2");
                 });
