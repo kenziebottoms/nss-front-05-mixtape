31055b5ed8278a1d8851510ac02d0987fd64dfc5
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-02-12 13:14:45 -0600

    only cache media if store info is more than 24 hours old

--- assets/js/ctrl/movie.js
+++ assets/js/ctrl/movie.js
@@ -4,17 +4,17 @@ const angular = require("angular");
 
 angular.module("mixtape").controller("MovieCtrl", function($scope, TmdbFactory, $routeParams, TMDB, LinkFactory, FirebaseFactory) {
     $scope.id = $routeParams.id;
+    let typeId = `movie:${$scope.id}`;
     TmdbFactory.getMovieById($scope.id)
         .then(movie => {
-            let typeId = `movie:${movie.id}`;
             // update cached info in Firebase
             movie =  TmdbFactory.parseApiInfo("movie", movie);
             FirebaseFactory.storeMedia(typeId, movie);
 
             // pass data to dom
             $scope.media = movie;
-            return LinkFactory.getLinksByMedia(typeId);
-        })
+        });
+    LinkFactory.getLinksByMedia(typeId)        
         .then(loadedLinks => {
             $scope.links = loadedLinks;
             $scope.context = "media";
