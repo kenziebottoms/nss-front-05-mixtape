eed53186d074fc49547c1f2bccbeec34280b3d69
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-02-13 11:31:29 -0600

    your votes populate on load

--- assets/js/ctrl/media/media.js
+++ assets/js/ctrl/media/media.js
@@ -57,18 +57,22 @@ angular.module("mixtape").controller("MediaCtrl", function($scope, $routeParams,
     $scope.upvote = linkId => {
         let link = $scope.links.find(link => link.key == linkId);
         if (link.vote == 1) {
-            VoteFactory.unvote(linkId, $scope.user.id);
+            VoteFactory.unvote(linkId, $scope.user.id)
+                .then(response => link.vote = 0);
         } else {
-            VoteFactory.upvote(linkId, $scope.user.id);
+            VoteFactory.upvote(linkId, $scope.user.id)
+                .then(response => link.vote = 1);
         }
     };
 
     $scope.downvote = linkId => {
         let link = $scope.links.find(link => link.key == linkId);
         if (link.vote == -1) {
-            VoteFactory.unvote(linkId, $scope.user.id);
+            VoteFactory.unvote(linkId, $scope.user.id)
+                .then(response => link.vote = 0);
         } else {
-            VoteFactory.downvote(linkId, $scope.user.id);
+            VoteFactory.downvote(linkId, $scope.user.id)
+                .then(response => link.vote = -1);
         }
     };
 });
