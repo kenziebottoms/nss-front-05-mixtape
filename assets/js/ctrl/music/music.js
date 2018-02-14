22d2759e808e9267609394c124947aec2e880bc8
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-02-14 14:10:34 -0600

    notate & prune music ctrls

--- assets/js/ctrl/music/music.js
+++ assets/js/ctrl/music/music.js
@@ -2,10 +2,12 @@
 
 const angular = require("angular");
 
-angular.module("mixtape").controller("MusicCtrl", function($scope, $q, $controller, $location, VoteFactory, SpotifyAuthFactory, LinkFactory) {
+angular.module("mixtape").controller("MusicCtrl", function($scope, $q, $controller, LinkFactory) {
     
+    // gets voting, deletion, playback methods from LinkCardCtrl
     $controller("LinkCardCtrl", {$scope: $scope});
 
+    // promises list of all loaded links connected to this piece of music
     $scope.getLinks = () => {
         return $q((resolve, reject) => {
             LinkFactory.getLinksByMusic($scope.typeId)
